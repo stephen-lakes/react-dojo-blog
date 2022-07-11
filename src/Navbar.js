59b9ee13e6d84417a16b618e497511e3239d3d94
stephen-lakes <oluyomiolamilekan99@gmail.com>
2022-07-11 11:58:48 +0100

    initial commit

---
+++ src/Navbar.js
@@ -0,0 +1,16 @@
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <h1><a href="/">The Dojo Blog</a></h1>
+            <div className="links">
+                <Link to="/">Home</Link>
+                <Link to="/create">New Blog</Link>
+            </div>
+        </nav>
+
+    );
+}
+
+export default Navbar;
